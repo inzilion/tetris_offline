e41bf4811e9046319b81d0c4ba0029bb58fcb3b0
inzilion <inzilion@gmail.com>
2023-05-08 09:44:48 +0900

    Cell 클래스 만들기 시작

--- tetris.js
+++ tetris.js
@@ -8,36 +8,42 @@ const CELL_SIZE = 30;
 
 const cell = (i, j, color) =>{
   ctx.fillStyle = color;
-  ctx.fillRect(10+CELL_SIZE*i,10+CELL_SIZE*j,CELL_SIZE,CELL_SIZE);
+  ctx.fillRect(CELL_SIZE*i,CELL_SIZE*j,CELL_SIZE,CELL_SIZE);
   ctx.strokeStyle = '#fff';
-  ctx.strokeRect(10+CELL_SIZE*i,10+CELL_SIZE*j,CELL_SIZE,CELL_SIZE);
+  ctx.strokeRect(CELL_SIZE*i,CELL_SIZE*j,CELL_SIZE,CELL_SIZE);
 }
 
-for(let j=0; j<20; j++){
-  for(let i=0; i<10; i++){
-    cell(i,j,'#444');
-  }
-}
+// for(let j=0; j<20; j++){
+//   for(let i=0; i<10; i++){
+//     cell(i,j,'#444');
+//   }
+// }
 
-const draw = () =>{
-  console.log("속았지")
-}
+// const draw = () =>{
+//    console.log("전역 함수")
+// }
 
 class Cell {
-  constructor(color){
-    this.color = color;     
+  constructor(size, color){
+    this.size = size;
+    this.color = color;
   }
-
-  draw(){
-    console.log(this.color);
-  }
-  draw2(){
-    this.draw();
+  draw(pos={x, y}){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(pos.x, pos.y, this.size, this.size);
+    ctx.strokeStyle = '#fff';
+    ctx.strokeRect(pos.x, pos.y, this.size, this.size);  
   }
 }
 
-const c = new Cell("red");
-const d = new Cell("yellow");
+const c = new Cell(30, '#444');
+c.draw({x:0,y:0});
 
-c.draw2();
-d.draw2();
+
+
+
+
+// const d = new Cell("yellow");
+
+// c.draw2();
+// d.draw2();
